3d83c51341210bc0afd350c9c994f1dcbf3b3ceb
Artem Usmanov <drfreeza5@gmail.com>
2021-12-01 22:57:16 +0700

    4th commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace ASU
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Класс для Listview
         public class ItemListBox
         {
             public string TypeOfFunc { get; set; }
@@ -33,6 +34,21 @@ namespace ASU
             }
         }
 
+        //Класс, с помощбю которого можно создать список значений, которые будут автоматически заполнятся, если пользователь меняет функцию
+        public class MemoryListBox
+        {
+            public string A { get; set; }
+            public string B { get; set; }
+            public MemoryListBox(string A, string B)
+            {
+                this.A = A;
+                this.B = B;
+            }
+        }
+        
+        public List<MemoryListBox> memoryList = new List<MemoryListBox>();//Список, в котором хранятся данные полей (для атоматического заполнения при изменении функции)
+        
+        //Класс для Datagrid
         public class ItemDGBox
         {
             public string func_xy { get; set; }
@@ -46,24 +62,37 @@ namespace ASU
                 this.y = y;
             }
         }
-        public List<ItemDGBox> itemsDG = new List<ItemDGBox>();
+        
+        public List<ItemDGBox> itemsDG = new List<ItemDGBox>();//Список item-ов датагрида
         public MainWindow()
         {
-            InitializeComponent();
-            List<ItemListBox> itemsListBox = new List<ItemListBox>();
+            InitializeComponent();           
+            List<ItemListBox> itemsListBox = new List<ItemListBox>();//Список item-ов listview
+
+            //Добавление в список функций
             itemsListBox.Add(new ItemListBox("Линейная", "f(x, y) = ax + by° + c"));
             itemsListBox.Add(new ItemListBox("Квадратичная", "f(x, y) = ax² + by + c"));
             itemsListBox.Add(new ItemListBox("Кубическая", "f(x, y) = ax³ + by² + c"));
             itemsListBox.Add(new ItemListBox("4-ой степени", "f(x, y) = ax⁴ + by³ + c"));
             itemsListBox.Add(new ItemListBox("5-ой степени", "f(x, y) = ax⁵ + by⁴ + c"));
-            LBox.ItemsSource = itemsListBox.ToList();
-            itemsDG.Add(new ItemDGBox("", "", ""));
-            DGBox.ItemsSource = itemsDG.ToList();
+            
+            LBox.ItemsSource = itemsListBox.ToList();//Вывод списка в listview            
+            itemsDG.Add(new ItemDGBox("", "", ""));//Добавление нового пустого айтема для датагрид
+            DGBox.ItemsSource = itemsDG.ToList();//Вывод пустого айтема в датагрид, чтобы пользователь мог ввести x и y
+
+            //Заполнение списка данных полей начальными значениями 
+            for (int i = 0; i < 5; i++)
+            {
+                memoryList.Add(new MemoryListBox("Введите A", "Введите B"));
+            }
+
+            LBox.SelectedIndex = 0;//Начальная выбранная функция - линейная
         }
 
         private void ABox_GotFocus(object sender, RoutedEventArgs e)
         {
-            if(ABox.Text == "Введите A")
+            //Логика маски для поля А
+            if (ABox.Text == "Введите A")
             {
                 ABox.Text = "";
             }
@@ -71,14 +100,15 @@ namespace ASU
 
         private void ABox_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (ABox.Text == "" || ABox.Text == null)
-            {
-                ABox.Text = "Введите A";
-            }
+            //if (ABox.Text == "" || ABox.Text == null)
+            //{
+            //    ABox.Text = "Введите A";
+            //}
         }
 
         private void BBox_GotFocus(object sender, RoutedEventArgs e)
         {
+            //Логика маски для поля В
             if (BBox.Text == "Введите B")
             {
                 BBox.Text = "";
@@ -87,14 +117,15 @@ namespace ASU
 
         private void BBox_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (BBox.Text == "" || ABox.Text == null)
-            {
-                BBox.Text = "Введите B";
-            }
+            //if (BBox.Text == "" || ABox.Text == null)
+            //{
+            //    BBox.Text = "Введите B";
+            //}
         }
 
         private void CBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //Логика маски для выпадющего списка
             if (CBox.SelectedIndex != -1)
             {
                 CLabel.Visibility = Visibility.Hidden;
@@ -107,6 +138,7 @@ namespace ASU
 
         private void LBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //При изменнении функции автоматически меняются значения в выпадающем списке (1, 10, 100 и тд)
             switch (LBox.SelectedIndex)
             {
                 case 0:
@@ -164,7 +196,10 @@ namespace ASU
                         CBox.SelectedIndex = ind;
                         break;
                     }
-            }    
+            }
+            //Заполняем поля данными из списка данных для автоматического заполнения
+            ABox.Text = memoryList[LBox.SelectedIndex].A;
+            BBox.Text = memoryList[LBox.SelectedIndex].B;
         }
 
         private void DGBox_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
@@ -174,43 +209,47 @@ namespace ASU
 
         private void DGBox_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
         {
+            //Вытаскиваем значения из полей x и y
             var rowcontent = DGBox.Columns[1].GetCellContent(DGBox.SelectedItem);
             var rowcontent1 = DGBox.Columns[2].GetCellContent(DGBox.SelectedItem);
-            if (rowcontent.Parent.ToString().Length != 36 && rowcontent1.Parent.ToString().Length != 36)
+            if (rowcontent.Parent.ToString().Length != 36 && rowcontent1.Parent.ToString().Length != 36)//Если оба поля не пустые
             {
-                if (LBox.SelectedItem == null || LBox.SelectedIndex < 0)
+                if (LBox.SelectedItem == null || LBox.SelectedIndex < 0)//Если не выбрана функция
                 {
                     MessageBox.Show("Не выбрана функция!");
                     DGBox.ItemsSource = itemsDG.ToList();
                 }
                 else
                 {
-                    if (ABox.Text == "" || BBox.Text == "" || CBox.SelectedItem == null || CBox.SelectedIndex < 0)
+                    if (ABox.Text == "" || BBox.Text == "" || CBox.SelectedItem == null || CBox.SelectedIndex < 0)//Если не заполнены поля А, В и С
                     {
                         MessageBox.Show("Введены не все коэффиценты!");
                         DGBox.ItemsSource = itemsDG.ToList();
                     }
                     else
                     {
+                        //Приводим вытащенные значения в приемлемый вид
                         string CellX = rowcontent.Parent.ToString().Remove(0, 38);
                         string CellY = rowcontent1.Parent.ToString().Remove(0, 38);
                         int x, y;
+                        //Парсим значения
                         int.TryParse(CellX, out x);
                         int.TryParse(CellY, out y);
-                        if ((x == 0 && CellX != "0") || (y == 0 && CellY != "0"))
+                        if ((x == 0 && CellX != "0") || (y == 0 && CellY != "0"))//Проверка на то, чтобы в поля x и y не было введено некорректных символов
                         {
                             MessageBox.Show("X или Y введены неверно!");
                         }
                         else
                         {
+                            //добавление значений в список для датагрида
                             itemsDG[DGBox.SelectedIndex].x = rowcontent.Parent.ToString().Remove(0, 38);
                             itemsDG[DGBox.SelectedIndex].y = rowcontent1.Parent.ToString().Remove(0, 38);
                             itemsDG[DGBox.SelectedIndex].func_xy = (Convert.ToInt32(ABox.Text) * Math.Pow(x, LBox.SelectedIndex + 1) + Convert.ToInt32(BBox.Text) * Math.Pow(y, LBox.SelectedIndex) + Convert.ToInt32(CBox.Text)).ToString();
-                            if (DGBox.SelectedIndex == itemsDG.Count - 1)
+                            if (DGBox.SelectedIndex == itemsDG.Count - 1)//если строка, которую пользователь изменял была последней в датагриде
                             {
-                                itemsDG.Add(new ItemDGBox("", "", ""));
+                                itemsDG.Add(new ItemDGBox("", "", ""));//то добавить в список новую пустую функцию
                             }
-                            DGBox.ItemsSource = itemsDG.ToList();
+                            DGBox.ItemsSource = itemsDG.ToList();//выводим все функции из списка в датагрид
                         }
                     }
                 }
@@ -223,17 +262,19 @@ namespace ASU
             {
                 string str = ABox.Text;
                 int a;
-                int.TryParse(ABox.Text, out a);
-                if (a == 0 && ABox.Text != "0")
+                int.TryParse(ABox.Text, out a);//Парсим введённое значение
+                if (a == 0 && ABox.Text != "0")//Если введённое значение содержит некорректные символы
                 {
-                    ABox.Text = str.Substring(0, str.Length - 1);
-                    ABox.CaretIndex = ABox.Text.Length;
+                    ABox.Text = str.Substring(0, str.Length - 1); //то удалить последний введённы символ
+                    ABox.CaretIndex = ABox.Text.Length;//и перенести каретку в конец поля
                 }
+                memoryList[LBox.SelectedIndex].A = ABox.Text;//Запись введённого значения в список данных для автоматического заполнения полей
             }
         }
 
         private void BBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            //Всё то же самое, что и в ABox_TextChanged
             if (BBox.Text != "Введите B" && BBox.Text != "")
             {
                 string str = BBox.Text;
@@ -244,7 +285,27 @@ namespace ASU
                     BBox.Text = str.Substring(0, str.Length - 1);
                     BBox.CaretIndex = BBox.Text.Length;
                 }
+                memoryList[LBox.SelectedIndex].B = BBox.Text;
             }
+        }
+
+        public int Testing(int index_l, int a, int b, int index_c, int x, int y)
+        {
+            int ind_of_res = itemsDG.Count - 1;
+            LBox.SelectedIndex = index_l;
+            ABox.Text = a.ToString();
+            BBox.Text = b.ToString();
+            CBox.SelectedIndex = index_c;
+            itemsDG[ind_of_res].x = x.ToString();
+            itemsDG[ind_of_res].y = y.ToString();
+            //До этого момента логика работает правильно, дальше что-то ломается
+            //Неизвестно, заполняется ли datagrid, потому что не получается его прочитать, хотя сделано всё также, как и в основном коде
+            DGBox.ItemsSource = itemsDG.ToList();
+            DGBox.SelectedIndex = ind_of_res;
+            DGBox_CellEditEnding(null, null);
+            var rowcontent = DGBox.Columns[0].GetCellContent(DGBox.Items[ind_of_res]);
+            int result = Convert.ToInt32(rowcontent.Parent.ToString().Remove(0, 38));
+            return result;
         }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ASU;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Тесты не проходят, проверял несколько раз правильность написания логики для тестов, всё должно работать.
+        //Скорее всего не получается из-за того, что при выполнении тестов невозможно заполнить и/или прочитать Datagrid
+        [TestMethod]
+        public void TestFunc1()
+        {
+            MainWindow window = new MainWindow();
+            Assert.AreEqual(47, window.Testing(0, 6, 2, 2, 7, 2));
+        }
+
+        [TestMethod]
+        public void TestFunc2()
+        {
+            MainWindow window = new MainWindow();
+            Assert.AreEqual(111, window.Testing(1, 9, 4, 0, 3, 5));
+        }
+
+        [TestMethod]
+        public void TestFunc3()
+        {
+            MainWindow window = new MainWindow();
+            Assert.AreEqual(15034, window.Testing(2, 8, 6, 4, 10, 33));
+        }
+
+        [TestMethod]
+        public void TestFunc4()
+        {
+            MainWindow window = new MainWindow();
+            Assert.AreEqual(49252, window.Testing(3, 4, 11, 3, 9, 12));
+        }
+
+        [TestMethod]
+        public void TestFunc5()
+        {
+            MainWindow window = new MainWindow();
+            Assert.AreEqual(2298132, window.Testing(4, 3, 7, 1, 15, 1));
+        }
+    }
+}
